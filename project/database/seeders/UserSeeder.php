e183b362a0d7ae32f620bf16aee22acb88529f59
Raymond920 <139645244+Raymond920@users.noreply.github.com>
2025-03-14 17:49:34 +0800

    doing login form

---
+++ project/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        for($i = 1; $i < 10; $i++){
+            DB::table('user')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@outlook.com',
+                'password' => bcrypt('password123'), // secure password
+            ]);
+        }
+    }
+}
