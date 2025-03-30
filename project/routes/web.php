8a993969f575e145a727d0ba97256b2d799f3775
Raymond <kokyuhang2004@gmail.com>
2025-03-30 16:27:16 +0800

    Added header component and home page

--- project/routes/web.php
+++ project/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/welcome', function () {
     return view('welcome');
@@ -15,3 +16,5 @@ Route::post('/login', [LoginController::class, 'login']);
 
 
 Route::get('/', [LoginController::class, 'printUserTable']);
+
+Route::get('/home', [HomeController::class, 'showHomePage']);
