3765c47498d84e3605126d777b7a025fda480aa1
Raymond <kokyuhang2004@gmail.com>
2025-03-30 17:47:41 +0800

    navbar half-progress

--- project/resources/views/components/side-navbar.blade.php
+++ project/resources/views/components/side-navbar.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+<link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+
+<div class="side-navbar" id="sidebar">
+    <button class="close-btn" onclick="toggleSidebar()">✖</button>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
 </div>
