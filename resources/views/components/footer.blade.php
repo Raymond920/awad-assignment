f3aaf902b99b963caa142f1f0270e9ee76f323cd
Raymond <kokyuhang2004@gmail.com>
2025-03-31 14:54:14 +0800

    done css for side-navbar and right content. similar to reddit website

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,3 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-    <p>This is footer</p>
-</div>
+<footer style="margin-top: auto;">
+    <p style="margin: 0;">This is footer</p>
+</footer>
