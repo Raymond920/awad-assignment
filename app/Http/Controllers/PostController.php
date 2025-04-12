be8605f1744cfbf6bea7eda8cc2623faf8b4ca09
Raymond <kokyuhang2004@gmail.com>
2025-04-13 01:36:42 +0800

    adjust home blade css and add new font

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,9 +6,18 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    // this method is just for testing can delete later
     public function show($id)
     {
         $post = Post::find($id);  // Retrieve the post by its ID
         return view('testing', compact('post'));  // Pass the post to the 'home' view
+    }
+
+    public function index(){
+        // fetch all posts from database
+        $posts = Post::all();
+
+        // pass the posts data to home view
+        return view('home', compact('posts'));
     }
 }
